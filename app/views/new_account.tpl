dfb7c90e9d4dff2a96f5befbbda3cf9d0b0688c8
Marcos de Paiva Branco Junior <eu.marcos.paiva@gmail.com>
2009-12-16 16:04:45 -0200

    Creation of pages of new account and contact.

---
+++ app/views/new_account.tpl
@@ -0,0 +1,20 @@
+<div id="new_account" class="container_12 clearfix">
+	<form action="" method="post">
+			<fieldset>
+				<legend>Digite os dados abaixo corretamente</legend>
+				<label for="name">Nome completo:</label>
+				<input type="text" id="name">
+				<label for="email">E-mail:</label>
+				<input type="text" id="email">
+				<label for="password">Senha:</label>
+				<input type="text" id="password">
+				<label for="password2">Repita a senha:</label>
+				<input type="text" id="password2">
+				<label for="cep">Seu CEP:</label><a href="#">Porque essa informação é iportante?</a>
+				<input type="text" id="cep">
+				<img src="" alt=""><label for="capitcha">o que você vê:</label><input type="text" id="capitcha">
+		</fieldset>
+		<a href="#">Veja os termos de uso</a>
+		<input type="button" value="Crie a minha conta">
+	</form>
+</div>
---
+++ app/views_c/%%E9^E93^E93BEBDA%%contact.tpl.php
@@ -0,0 +1,24 @@
+<?php /* Smarty version 2.6.22, created on 2009-12-16 16:02:51
+         compiled from contact.tpl */ ?>
+<div id="contact" class="container_12 clearfix">
+	<form action="" method="post">
+			<fieldset>
+				<legend>Você pode fazer contato através do formulário abaixo:</legend>
+				<label for="name">Nome completo:</label>
+				<input type="text" id="name">
+				<label for="email">E-mail:</label>
+				<input type="text" id="email">
+				<label for="subject">Assunto</label>
+				<select id="subject">
+					<option>Sugestão</option>
+					<option>Bla bla bla</option>
+					<option>Sugestão</option>
+				</select>
+				<img src="" alt=""><label for="capitcha">o que você vê:</label><input type="text" id="capitcha">
+				<label for="text">Seu texto</label>
+				<textarea id="text" cols="30" rows="10"></textarea>
+		</fieldset>
+		<a href="#">Veja os termos de uso</a>
+		<input type="button" value="Crie a minha conta">
+	</form>
+</div>
