f5de6a7881dfac9f3efd6e4415944f2ad1786c5f
Marcos de Paiva Branco Junior <eu.marcos.paiva@gmail.com>
2010-02-09 08:01:55 -0200

    Modificação do arquivo .gitignore

--- app/views_c/%%29^292^2922C27E%%common_rightbox.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php /* Smarty version 2.6.22, created on 2009-12-17 15:22:38
-         compiled from defult/common_rightbox.tpl */ ?>
-<p id="common_rightbox" class="grid_6"><a href="">Faça o login</a> ou <a href="">Crie uma conta</a></p>
-<!--o próximo bloco é setado como visivel se o usuário estiver logado.-->
-<p id="common_rightbox_loged">Olá <a href="">Marcos de Paiva<a> - <a href="">logout</a></p>
