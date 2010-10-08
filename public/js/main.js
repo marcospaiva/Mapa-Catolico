ad61be6e4df473b2309dd9f6e0289f9474065c09
Marcos de Paiva Branco Junior <eu.marcos.paiva@gmail.com>
2010-10-08 14:11:12 -0300

    Retirando suporte antigo a tooltip, colocando modalbox para login

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,43 @@
-/*#### ISSO MANIPULA O COMPORTAMENTO GERAL DO FRONT####*/
+$(document).ready(function() {
+	//seleciona os elementos a com atributo name="modal"
+	$('a[name=modal]').click(function(e) {
+		//cancela o comportamento padrão do link
+		e.preventDefault();
 
-/*TOOLTIP*/
-function open(){
-	$(".tooltip").css("display", "block"); 
-};
-function close(){
-	$(".tooltip").css("display", "none"); 
-};
+		//armazena o atributo href do link
+		var id = $(this).attr('href');
 
-$(document).ready(function(){
-   $("#login a").click(function(){
-      open();
-   });
-$(".tooltip a").click(function(){
-      close();
-   });
+		//armazena a largura e a altura da tela
+		var maskHeight = $(document).height();
+		var maskWidth = $(window).width();
+
+		//Define largura e altura do div#mask iguais ás dimensçoes da tela
+		$('#mask').css({'width':maskWidth,'height':maskHeight});
+
+		//efeito de transição
+		$('#mask').fadeIn(500);
+		$('#mask').fadeTo("fast",0.8);
+
+		//armazena a largura e a altura da janela
+		var winH = $(window).height();
+		var winW = $(window).width();
+		//centraliza na tela a janela popup
+		$(id).css('top',  winH/2-$(id).height()/2);
+		$(id).css('left', winW/2-$(id).width()/2);
+		//efeito de transição
+		$(id).fadeIn(500);
+	});
+
+//se o botãoo fechar for clicado
+$('.window .close').click(function (e) {
+	//cancela o comportamento padrão do link
+	e.preventDefault();
+	$('#mask, .window').hide();
+});
+
+//se div#mask for clicado
+$('#mask').click(function () {
+$(this).hide();
+$('.window').hide();
+});
 });
