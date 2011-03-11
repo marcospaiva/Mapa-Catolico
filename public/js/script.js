406664871aee5135b0181aea99bd7c50ccf0b1bb
Marcos Paiva <eu.marcos.paiva@gmail.com>
2011-03-11 15:50:38 -0300

    Instituição de arquivos padrão CSS e JS

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,10 @@
 $(function(){
 	
-	        $(".slider").carousel( { pagination: true, direction: "vertical",autoSlide: true,
+	        $(".slider").carousel( {pagination: true, direction: "vertical",autoSlide: true,
 			        autoSlideInterval: 5000 } );
 	   		
 			$(".sign_in").click(function(){
+			
 			$(this).toggleClass("active");
 				$("#sign_box").toggle();
 				return false;
