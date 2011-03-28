9f07da431bfab79dbec2787d39ab34ec917c8b1f
Luciana Mendes Gonçalves <super.luci@gmail.com>
2011-03-28 14:06:26 -0300

    troquei o endereco do RSS do twitter para testar

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -21,7 +21,7 @@ class IndexController extends Zend_Controller_Action {
  	
 
 	try{
-		$channel = new Zend_Feed_Rss('http://podcast.cancaonova.com/rss/categoria.xml.php?idCategoria=101');
+		$channel = new Zend_Feed_Rss('http://twitter.com/statuses/user_timeline/140888598.rss');
 		foreach ($channel as $item) {
 			$title[] = $item->title();
 		}
